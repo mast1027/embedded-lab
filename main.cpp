0ffc425aef5e62d556a268f45412cc28e75df978
Stefan Maier <stefan.maier@formled.com>
2024-04-05 14:35:40 +0200

    show size of read data

--- main.cpp
+++ main.cpp
@@ -45,8 +45,6 @@ int main(int argc, char *argv[]) {
     file.close(); // Close the file
 
     if (DEBUG_MODE) {
-        //get length of receivedData
-
         // print parsed receivedData
         std::cout << "Parsed receivedData[" << receivedData.size() << "]:" << std::endl;
         for (int i = 0; i < receivedData.size(); i++) {
