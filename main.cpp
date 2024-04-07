45e2710eda94638a42d94c208c005b65b2b8a862
Stefan Maier <stefan.maier@formled.com>
2024-04-07 17:50:30 +0200

    update main to solve assignemnt

--- main.cpp
+++ main.cpp
@@ -9,17 +9,20 @@ int main(int argc, char *argv[]) {
     // Setup Satellite Network
     SatelliteNetwork network = SatelliteNetwork();
 
+    // print satellite network
     if (DEBUG_MODE) {
+        std::cout << "############ Satellite Infos ###########" << std::endl;
         for (int i = 0; i < network.getNumSatellites(); i++) {
             std::cout << "Satellite "
                       << std::setw(2)    // Set the width to 2 characters
                       << std::setfill(' ') // Fill with ' ' if the number has less than 2 characters
-                      << i + 1 << " = ";
-            SatelliteNetwork::getSatellite(i)->printRegistersPositions();
+                      << network.getSatellite(i)->getSatelliteId() + 1 << " = ";
+            network.getSatellite(i)->printRegistersPositions();
             std::cout << "\t";
-            SatelliteNetwork::getSatellite(i)->printChipSequence();
+            network.getSatellite(i)->printChipSequence();
             std::cout << std::endl;
         }
+        std::cout << std::endl;
     }
 
     // read commandline arguments
@@ -43,13 +46,52 @@ int main(int argc, char *argv[]) {
     }
     file.close(); // Close the file
 
+    // print received data
     if (DEBUG_MODE) {
-        // print parsed receivedData
-        std::cout << "Parsed receivedData[" << receivedData.size() << "]:" << std::endl;
+        std::cout << "############ Received Data ###########" << std::endl;
         for (int i: receivedData) {
             std::cout << i << " ";
         }
+        std::cout << std::endl << std::endl;
+    }
+
+
+    if (DEBUG_MODE) {
+        std::cout << "############ Correlation Results ###########" << std::endl;
+    }
+    // cross-correlate all satellites
+    for (int i = 0; i < network.getNumSatellites(); i++) {
+        //ToDo: optimize this part by parallelizeing the for loop on n cores
+        network.getSatellite(i)->crossCorrelate(receivedData);
+        if (DEBUG_MODE) {
+            std::cout << "Satellite " << i + 1 << " correlation result: ";
+            std::cout << " Correlation: "
+                      << network.getSatellite(i)->getLastCorrelationResult().correlationValue
+                      << " Delta: " << network.getSatellite(i)->getLastCorrelationResult().delta << std::endl;
+        }
+    }
+    if (DEBUG_MODE) {
         std::cout << std::endl;
+    }
+
+    // get satellites with highest correlation
+    std::vector<Satellite *> bestSatellites = network.getSatellitesWithHighestCorrelation();
+    // print results like in the assignment
+    for (auto &bestSatellite: bestSatellites) {
+        std::cout << "Satellite "
+                  << std::setw(2)
+                  << std::setfill(' ')
+                  << bestSatellite->getSatelliteId() + 1 << " ";
+
+        if (bestSatellite->getLastCorrelationResult().correlationValue > 0) {
+            std::cout << " has sent bit 1 (delta = " << bestSatellite->getLastCorrelationResult().delta
+                      << ")" << std::endl;
+        } else if (bestSatellite->getLastCorrelationResult().correlationValue < 0) {
+            std::cout << " has sent bit 0 (delta = " << bestSatellite->getLastCorrelationResult().delta
+                      << ")" << std::endl;
+        } else {
+            std::cout << "No correlation found." << std::endl;
+        }
     }
 
 
